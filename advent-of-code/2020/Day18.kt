2beeb5ffb892e3f6ae051cb19c96582f9bcd95a8
Simon Naumov <daretoodefy@gmail.com>
2020-12-18 18:41:27 +0300

    :sparkles: advent of code 2020: day 18

---
+++ advent-of-code/2020/Day18.kt
@@ -0,0 +1,10 @@
+/**
+ * Simply ran my LALR parser generator (https://github.com/nothingelsematters/lalr-generator/)
+ * on files Day18Part1.gr and Day18Part2.gr and used the result.
+ */
+fun main() {
+    val expressions = System.`in`.reader().readText()
+    val first = DayEighteenPartOneParser().parse(expressions.byteInputStream())
+    val second = DayEighteenPartTwoParser().parse(expressions.byteInputStream())
+    println("$first $second")
+}
