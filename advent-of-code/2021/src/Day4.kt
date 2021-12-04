1848d1073bfdb0ab9752d9bf87bcd34a18a4f7ba
Simon Naumov <daretoodefy@gmail.com>
2021-12-04 09:08:16 +0300

    :sparkles: advent of code 2021: day 4

---
+++ advent-of-code/2021/src/Day4.kt
@@ -0,0 +1,32 @@
+private fun findWinner(numbers: List<Int>, boards: List<List<List<Int>>>, position: Int): Int {
+    val marked = mutableSetOf<Int>()
+    val won = mutableSetOf<Int>()
+
+    for (number in numbers) {
+        marked.add(number)
+
+        for ((i, board) in boards.withIndex()) {
+            if (i in won) continue
+
+            val isRowMarked = board.any { row -> row.all { it in marked } }
+            val isColumnMarked = board.indices.any { j -> board.all { it[j] in marked } }
+
+            if (isRowMarked || isColumnMarked) {
+                won.add(i)
+                if (won.size == position) {
+                    return number * board.flatten().filter { it !in marked }.sum()
+                }
+            }
+        }
+    }
+
+    throw IllegalArgumentException("Invalid input")
+}
+
+fun main() {
+    val numbers = readln().toInts(",")
+    val boards = mapBlocks { it.toInts() }
+
+    println(findWinner(numbers, boards, 1))
+    println(findWinner(numbers, boards, boards.size))
+}
--- advent-of-code/2021/src/Utils.kt
+++ advent-of-code/2021/src/Utils.kt
@@ -2,3 +2,14 @@ private fun inputBufferedReader() = System.`in`.bufferedReader()
 
 fun <T> mapLines(mapper: (String) -> T): MutableList<T> =
     inputBufferedReader().useLines { lines -> lines.map(mapper).toMutableList() }
+
+fun <T> mapBlocks(mapper: (String) -> T): MutableList<List<T>> =
+    inputBufferedReader()
+        .readText()
+        .trim('\n')
+        .splitToSequence("\n\n")
+        .map { it.lineSequence().map(mapper).toList() }
+        .toMutableList()
+
+fun String.toInts(delimiter: String = " "): List<Int> =
+    splitToSequence(delimiter).filter { it.isNotEmpty() }.map { it.toInt() }.toList()
