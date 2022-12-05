3babd544021c2c871265bbc9bf3809fc1e282c4a
Simon Naumov <daretoodefy@gmail.com>
2022-12-05 15:30:58 +0600

    :sparkles: advent of code 2022: day 5

--- advent-of-code/2022/README.md
+++ advent-of-code/2022/README.md
@@ -2,15 +2,8 @@
 
 Recommended to use [aoc-cli](https://github.com/scarvalhojr/aoc-cli)
 
-- Day 1: Calorie Counting
-  - [Problem](https://adventofcode.com/2022/day/1)
-  - [Solution](src/Day01.kt)
-- Day 2: Rock Paper Scissors
-  - [Problem](https://adventofcode.com/2022/day/2)
-  - [Solution](src/Day02.kt)
-- Day 3: Rucksack Reorganization
-  - [Problem](https://adventofcode.com/2022/day/3)
-  - [Solution](src/Day03.kt)
-- Day 4: Camp Cleanup
-  - [Problem](https://adventofcode.com/2022/day/4)
-  - [Solution](src/Day04.kt)
+- [[Problem]](https://adventofcode.com/2022/day/1) [[Solution]](src/Day01.kt) Day 1: Calorie Counting
+- [[Problem]](https://adventofcode.com/2022/day/2) [[Solution]](src/Day02.kt) Day 2: Rock Paper Scissors
+- [[Problem]](https://adventofcode.com/2022/day/3) [[Solution]](src/Day03.kt) Day 3: Rucksack Reorganization
+- [[Problem]](https://adventofcode.com/2022/day/4) [[Solution]](src/Day04.kt) Day 4: Camp Cleanup
+- [[Problem]](https://adventofcode.com/2022/day/5) [[Solution]](src/Day05.kt) Day 5: Supply Stacks
---
+++ advent-of-code/2022/src/Day05.kt
@@ -0,0 +1,55 @@
+private data class Action(val count: Int, val from: Int, val to: Int)
+
+private fun readColumnsAndMoves(): Pair<MutableList<ArrayDeque<Char>>, List<Action>> {
+    val columns: MutableList<ArrayDeque<Char>> = mutableListOf()
+
+    while (true) {
+        val line = readln()
+        if ("[" !in line) break
+
+        line.asSequence()
+            .withIndex()
+            .filter { (index, _) -> index % 4 == 1 }
+            .map { (_, i) -> i }
+            .withIndex()
+            .onEach { (index, _) -> if (columns.getOrNull(index) == null) columns.add(ArrayDeque()) }
+            .filter { (_, i) -> !i.isWhitespace() }
+            .forEach { (index, i) -> columns[index].addFirst(i) }
+    }
+
+    readln()
+
+    val moves = mapLines {  action ->
+        val (move, from, to) = """move (\d+) from (\d+) to (\d+)""".toRegex()
+            .matchEntire(action)
+            .unwrap()
+            .destructured
+            .toList()
+            .map { it.toInt() }
+        Action(move, from - 1, to - 1)
+    }
+
+    return columns to moves
+}
+
+private fun assembleOutput(columns: Iterable<List<Char>>) = columns.joinToString("") { it.last().toString() }
+
+private fun <T> ArrayDeque<T>.removeLast(count: Int) = (0 until count).map { removeLast() }
+
+private fun part1(columns: MutableList<ArrayDeque<Char>>, moves: List<Action>): String {
+    moves.forEach { (count, from, to) -> columns[to].addAll(columns[from].removeLast(count)) }
+    return assembleOutput(columns)
+}
+
+private fun part2(columns: MutableList<ArrayDeque<Char>>, moves: List<Action>): String {
+    moves.forEach { (count, from, to) -> columns[to].addAll(columns[from].removeLast(count).asReversed()) }
+    return assembleOutput(columns)
+}
+
+fun main() {
+    val (columns, moves) = readColumnsAndMoves()
+    val columnsDeepCopy = columns.asSequence().map { ArrayDeque(it) }.toMutableList()
+
+    println(part1(columnsDeepCopy, moves))
+    println(part2(columns, moves))
+}
